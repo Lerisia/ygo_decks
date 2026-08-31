ca57cc9c23d66f03701294926f9b827d012b80d0
lerisia <rlawodus96@gmail.com>
2026-08-31 15:59:09 +0900

    feat: register tournament models in django admin

--- backend/tournament/admin.py
+++ backend/tournament/admin.py
@@ -5,21 +5,21 @@ from .models import Entrant, Match, Round, Tournament
 
 @admin.register(Tournament)
 class TournamentAdmin(admin.ModelAdmin):
-    list_display = ("name", "host", "format", "status", "capacity", "current_round", "event_date")
+    list_display = ("name", "format", "status", "host", "capacity", "current_round", "event_date")
     list_filter = ("format", "status")
     search_fields = ("name", "host__username")
 
 
 @admin.register(Entrant)
 class EntrantAdmin(admin.ModelAdmin):
-    list_display = ("name", "tournament", "status", "created_at")
+    list_display = ("name", "tournament", "status", "user")
     list_filter = ("status",)
     search_fields = ("name", "tournament__name")
 
 
 @admin.register(Round)
 class RoundAdmin(admin.ModelAdmin):
-    list_display = ("tournament", "number", "status", "created_at")
+    list_display = ("tournament", "number", "status", "random_seed")
 
 
 @admin.register(Match)
